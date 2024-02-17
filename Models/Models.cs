9a04d89aee35fcc992bd6fa57390b08b84727c48
Rukus Vitalina <v.rukus>
2024-02-17 19:10:49 +0300

    Добавьте файлы проекта.

---
+++ Models/Models.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
